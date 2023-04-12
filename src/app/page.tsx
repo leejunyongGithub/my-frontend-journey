b817fe93c2628f97e534e711a3af7a8a366d63b4
junyong <junyong0751@gmail.com>
2023-04-12 14:22:01 +0900

    폴더명 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { RecoilRoot } from "recoil";
-import Layout from "./components/templates/Layout/Layout";
+import Layout from "./components/templates/layout/Layout";
 
 export default function Home() {
   return (
